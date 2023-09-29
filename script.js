f8a07d1db2e513f06065c1914e9eae191b3564aa
EnzoTM1170 <enzotm117@gmail.com>
2023-09-29 09:30:53 -0300

    adicionando arquivo

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = "Enzo";
+let idade = 18;
+let status = true;
+let valorproduto = 10.50;
+let listatimes = ["time 1", "time 2", "time 3"];
+
+
+console.log(nome);
+console.log("sua idade é " + idade);
+console.log(status);
+console.log(valorproduto);
+console.log(listatimes);
+
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2; 
+let multiplicacao = num1 * num2; 
+let divisao = num1 / num2;
+
+console.log("O resultado da soma é "+ soma);
+console.log("O resultado da subtracao é "+ subtracao);
+console.log("O resultado da multiplicacao é "+ multiplicacao);
+console.log("O resultado da divisao é "+ divisao);
